565dda4cb84629773bb2f53e911629082533ba5b
bobisaperson1 <bfulham@bradyfulham.com>
2026-01-04 14:18:43 +1030

    0.0.7

--- custom_components/sapnmeterdata/__init__.py
+++ custom_components/sapnmeterdata/__init__.py
@@ -9,7 +9,7 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(
+def async_setup_entry(
     hass: core.HomeAssistant, entry: config_entries.ConfigEntry
 ) -> bool:
     """Set up platform from a ConfigEntry."""
